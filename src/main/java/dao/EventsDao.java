cee3bd9dd2424aa4af25dd65e4b79cf6cd398e91
Sunny Metkar <sunny.metkar@db.com>
2021-03-26 01:24:15 +0530

    first commit

--- src/main/java/dao/EventsDao.java
+++ src/main/java/dao/EventsDao.java
@@ -21,37 +21,7 @@ public class EventsDao implements IGenericDao<Event> {
 		this.connection = connection;
 	}
 
-	public Event findOne(long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	public List<Event> findAll() {
-		List<Event> events = new ArrayList<Event>();
-		try {
-
-			Statement stmt = connection.createStatement();
-			String query = "select * from events_tbl5;";
-			Logger.debug("Query " + query);
-
-			ResultSet result = stmt.executeQuery(query);
-			while (result.next()) {
-				Event event = new Event();
-				event.setId(result.getString("id"));
-				event.setDuration(result.getInt("duration"));
-				event.setType(result.getString("type"));
-				event.setHost(result.getString("host"));
-				event.setAlert(result.getBoolean("alert"));
-			}
-			stmt.close();
-
-		} catch (Exception e) {
-			Logger.error(e.getMessage());
-			e.printStackTrace();
-		}
-		return events;
-
-	}
 
 	public void create(Event event) {
 		try {
