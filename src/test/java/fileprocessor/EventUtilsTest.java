26bbc71df997fe7ba96c711cacb401a792304d38
Sunny Metkar <sunny.metkar@db.com>
2021-03-26 13:42:28 +0530

    first commit

--- src/test/java/fileprocessor/EventUtilsTest.java
+++ src/test/java/fileprocessor/EventUtilsTest.java
@@ -2,7 +2,6 @@ package fileprocessor;
 
 import static org.junit.Assert.fail;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -16,7 +15,8 @@ public class EventUtilsTest {
 	@Test
 	public void testAddOrUpdateEvent() {
 		Map<String, Event> idToEventMap = new ConcurrentHashMap<String, Event>();
-		EventUtils.addOrUpdateEvent(new LogEvent(), idToEventMap);
+		EventUtils.addOrUpdateEvent(getTestLogEvent(), idToEventMap);
+		assert(idToEventMap.size()==1);
 	}
 
 	@Test
@@ -29,4 +29,22 @@ public class EventUtilsTest {
 		fail("Not yet implemented");
 	}
 
+	private Event getTestEvent(){
+		Event event=new Event();
+		event.setId("sun");
+		event.setDuration(3);
+		event.setHost("fads");
+		event.setType("application_log");
+		return event;
+	}
+	
+	private LogEvent getTestLogEvent(){
+		LogEvent logEvent=new LogEvent();
+		logEvent.setId("sun");
+		logEvent.setState("started");
+		logEvent.setTimestamp(5);
+		logEvent.setHost("fads");
+		logEvent.setType("application_log");
+		return logEvent;
+	}
 }
